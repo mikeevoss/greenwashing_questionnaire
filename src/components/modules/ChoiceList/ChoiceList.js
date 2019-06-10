e2697f4cb62bb514edf7ba0831f7338e7784cd6c
Wietse Neven <hello@wietseneven.com>
2019-06-10 18:30:28 +0200

    Allowing inputOptions as object

--- src/components/modules/ChoiceList/ChoiceList.js
+++ src/components/modules/ChoiceList/ChoiceList.js
@@ -3,11 +3,16 @@ import s from "./ChoiceList.module.css";
 
 const ChoiceList = ({ options, onChange }) => (
   <ul className={s.root}>
-    {options.map(option => (
-      <li key={option} className={s.item}>
-        <button onClick={() => onChange(option)} className={s.button}>{option}</button>
-      </li>
-    ))}
+    {options.map(option => {
+      const label = typeof option === "object" ? option.label : option;
+      return (
+        <li key={label} className={s.item}>
+          <button onClick={() => onChange(label)} className={s.button}>
+            {label}
+          </button>
+        </li>
+      );
+    })}
   </ul>
 );
 
